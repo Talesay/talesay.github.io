f22d8b563344d2aab7ea64e9f72b0b8a546c2081
FelipeBudinich <fbudinichd@gmail.com>
2015-06-14 22:19:08 -0400

    working on particles

--- kotr/lib/scene/run.js
+++ kotr/lib/scene/run.js
@@ -1,91 +1,91 @@
 /*global ig*/
 ig.module(
-    'scene.run'
+	'scene.run'
 ).requires(
-    'impact.game',
-    'scene.run.lvl.start',
-    'scene.run.lvl.segment-00',
-    'scene.run.lvl.segment-01',
-    'scene.run.lvl.segment-02',
-    'scene.run.lvl.segment-03',
-    'scene.run.lvl.segment-04',
-    'scene.run.lvl.segment-05',
-    'scene.run.lvl.segment-06',
-    'scene.run.lvl.segment-07',
-    'scene.run.lvl.other',
-    'scene.run.lvl.end'
+	'impact.game',
+	'scene.run.lvl.start',
+	'scene.run.lvl.segment-00',
+	'scene.run.lvl.segment-01',
+	'scene.run.lvl.segment-02',
+	'scene.run.lvl.segment-03',
+	'scene.run.lvl.segment-04',
+	'scene.run.lvl.segment-05',
+	'scene.run.lvl.segment-06',
+	'scene.run.lvl.segment-07',
+	'scene.run.lvl.other',
+	'scene.run.lvl.end'
 ).defines(function () {
-    'use strict';
-    ig.SceneRun = ig.Game.extend({
-        level: {},
-        gravity: 200,
-        nextCameraPosY: 0,
-        nextCameraPosX: 0,
-        cameraPosY: {},
-        cameraPosX: {},
-        init: function () {
-            this.infiniteLevel = new ig.InfiniteLevelManager(this.level);
-            this.initCamera();
-            ig.music.play('04');
-        },
-        initCamera: function () {
-            this.nextCameraPosY = this.player.pos.y / 2.5 - ig.system.height / 2 + this.player.size.y;
-            this.nextCameraPosX = this.player.pos.x - ig.system.width / 4;
-            this.getCameraPosY(this.nextCameraPosY, this.nextCameraPosY);
-            this.getCameraPosX(this.nextCameraPosX, this.nextCameraPosX, 1, ig.Interpolation.linear);
-        },
-        getCameraPosY: function (start, end) {
-            var duration = 1,
-                easing = ig.Interpolation.exponentialOut;
-            this.cameraPosY = new ig.Interpolation(start, end, duration, easing, function () {});
-        },
-        getCameraPosX: function (start, end, duration, easing) {
-            this.cameraPosX = new ig.Interpolation(start, end, duration, easing, function () {});
-        },
-        update: function () {
-            this.parent();
-            this.infiniteLevel.update();
-            this.camera();
-        },
-        camera: function () {
-            if (this.player.vel.x > 32 && this.player.standing && !this.player.attacked && this.player.standTime > 0.5) {
-                this.nextCameraPosX = this.player.pos.x + 256 - ig.system.width / 4;
-                this.getCameraPosX(this.cameraPosX.value, this.nextCameraPosX, 3, ig.Interpolation.exponentialIn);
-            } else if (this.player.standing && this.player.vel.x < 32) {
-                this.nextCameraPosX = this.player.pos.x + 64 - ig.system.width / 4;
-                this.getCameraPosX(this.cameraPosX.value, this.nextCameraPosX, 2, ig.Interpolation.linear);
-            } else {
-                this.nextCameraPosX = this.player.pos.x + 64 - ig.system.width / 4;
-                this.getCameraPosX(this.cameraPosX.value, this.nextCameraPosX, 1.1, ig.Interpolation.linear);
-            }
+	'use strict';
+	ig.SceneRun = ig.Game.extend({
+		level: {},
+		gravity: 200,
+		nextCameraPosY: 0,
+		nextCameraPosX: 0,
+		cameraPosY: {},
+		cameraPosX: {},
+		init: function () {
+			this.infiniteLevel = new ig.InfiniteLevelManager(this.level);
+			this.initCamera();
+			ig.music.play('04');
+		},
+		initCamera: function () {
+			this.nextCameraPosY = this.player.pos.y / 2.5 - ig.system.height / 2 + this.player.size.y;
+			this.nextCameraPosX = this.player.pos.x - ig.system.width / 4;
+			this.getCameraPosY(this.nextCameraPosY, this.nextCameraPosY);
+			this.getCameraPosX(this.nextCameraPosX, this.nextCameraPosX, 1, ig.Interpolation.linear);
+		},
+		getCameraPosY: function (start, end) {
+			var duration = 1,
+				easing = ig.Interpolation.exponentialOut;
+			this.cameraPosY = new ig.Interpolation(start, end, duration, easing, function () {});
+		},
+		getCameraPosX: function (start, end, duration, easing) {
+			this.cameraPosX = new ig.Interpolation(start, end, duration, easing, function () {});
+		},
+		update: function () {
+			this.parent();
+			this.infiniteLevel.update();
+			this.camera();
+		},
+		camera: function () {
+			if (this.player.vel.x > 32 && this.player.standing && !this.player.attacked && this.player.standTime > 0.5) {
+				this.nextCameraPosX = this.player.pos.x + 256 - ig.system.width / 4;
+				this.getCameraPosX(this.cameraPosX.value, this.nextCameraPosX, 3, ig.Interpolation.exponentialIn);
+			} else if (this.player.standing && this.player.vel.x < 32) {
+				this.nextCameraPosX = this.player.pos.x + 64 - ig.system.width / 4;
+				this.getCameraPosX(this.cameraPosX.value, this.nextCameraPosX, 2, ig.Interpolation.linear);
+			} else {
+				this.nextCameraPosX = this.player.pos.x + 64 - ig.system.width / 4;
+				this.getCameraPosX(this.cameraPosX.value, this.nextCameraPosX, 1.1, ig.Interpolation.linear);
+			}
 
-            if (this.player.standing) {
-                if (this.player.pos.y <= -14 && this.player.standTime > 0.1) {
-                    this.nextCameraPosY = this.player.pos.y / 5 - ig.system.height / 2 + this.player.size.y;
-                    this.getCameraPosY(this.cameraPosY.value, this.nextCameraPosY);
-                } else if (this.player.pos.y <= 2 && this.player.standTime > 0.1) {
-                    this.nextCameraPosY = this.player.pos.y / 4 - ig.system.height / 2 + this.player.size.y;
-                    this.getCameraPosY(this.cameraPosY.value, this.nextCameraPosY);
-                }
-            } else {
-                if (this.player.pos.y <= -32) {
-                    this.nextCameraPosY = this.player.pos.y * 0.2 - ig.system.height / 2 - this.player.size.y / 2;
-                    this.getCameraPosY(this.cameraPosY.value, this.nextCameraPosY);
-                } else {
-                    this.nextCameraPosY = this.player.pos.y / 5 - ig.system.height / 2 + this.player.size.y;
-                    this.getCameraPosY(this.cameraPosY.value, this.nextCameraPosY);
-                }
-            }
-            this.screen.y = this.cameraPosY.value;
-            if (this.screen.y < -62) {
-                this.screen.y = -62;
-            }
-            if (this.player.pos.y < 50) {
-                this.screen.x = this.cameraPosX.value;
-            }
-        },
-        draw: function () {
-            this.parent();
-        }
-    });
+			if (this.player.standing) {
+				if (this.player.pos.y <= -14 && this.player.standTime > 0.1) {
+					this.nextCameraPosY = this.player.pos.y / 5 - ig.system.height / 2 + this.player.size.y;
+					this.getCameraPosY(this.cameraPosY.value, this.nextCameraPosY);
+				} else if (this.player.pos.y <= 2 && this.player.standTime > 0.1) {
+					this.nextCameraPosY = this.player.pos.y / 4 - ig.system.height / 2 + this.player.size.y;
+					this.getCameraPosY(this.cameraPosY.value, this.nextCameraPosY);
+				}
+			} else {
+				if (this.player.pos.y <= -32) {
+					this.nextCameraPosY = this.player.pos.y * 0.2 - ig.system.height / 2 - this.player.size.y / 2;
+					this.getCameraPosY(this.cameraPosY.value, this.nextCameraPosY);
+				} else {
+					this.nextCameraPosY = this.player.pos.y / 5 - ig.system.height / 2 + this.player.size.y;
+					this.getCameraPosY(this.cameraPosY.value, this.nextCameraPosY);
+				}
+			}
+			this.screen.y = this.cameraPosY.value;
+			if (this.screen.y < -62) {
+				this.screen.y = -62;
+			}
+			if (this.player.pos.y < 50) {
+				this.screen.x = this.cameraPosX.value;
+			}
+		},
+		draw: function () {
+			this.parent();
+		}
+	});
 });
--- kotr/lib/scene/run/ent/player.js
+++ kotr/lib/scene/run/ent/player.js
@@ -1,125 +1,107 @@
 /*global ig*/
 ig.module(
-    'scene.run.ent.player'
+	'scene.run.ent.player'
 ).requires(
-    'impact.entity'
+	'impact.entity',
+	'scene.run.ent.particle.bounce-emitter',
+	'scene.run.ent.particle.land-emitter',
+	'scene.run.ent.particle.run-emitter',
+	'scene.run.ent.particle.backtrack-emitter',
+	'scene.run.ent.particle.draw-emitter',
+	'scene.run.ent.particle.jump-emitter'
 ).defines(function () {
-    'use strict';
-    window.EntityPlayer = ig.Entity.extend({
-        size: {
-            x: 14,
-            y: 14
-        },
-        offset: {
-            x: -1,
-            y: 3
-        },
-        maxVel: {
-            x: 72,
-            y: 900
-        },
-        vel: {
-            x: 64,
-            y: 0
-        },
-        accel: {
-            x: 32,
-            y: 0
-        },
-        bounceSpeed: 150,
-        jumpTime: 1,
-        standTime: 1,
-        breakTime: 1,
-        collisionCounter: 0,
-        airCollisionCounter: 0,
-        attacked: false,
-        jumped: false,
-        velGround: 32,
-        velAir: 32,
-        deathTimer: false,
-        type: ig.Entity.TYPE.A, // Player friendly group
-        checkAgainst: ig.Entity.TYPE.NONE,
-        collides: ig.Entity.COLLIDES.PASSIVE,
-        animSheet: new ig.AnimationSheet('med/spr/player.png', 16, 18),
-        deathFallSnd: new ig.Sound('med/sfx/death-fall.*'),
-        backtrackSnd: [
-            new ig.Sound('med/sfx/backtrack-00.*'),
-            new ig.Sound('med/sfx/backtrack-01.*'),
-            new ig.Sound('med/sfx/backtrack-02.*')
-        ],
-        bumpSnd: [
+	'use strict';
+	window.EntityPlayer = ig.Entity.extend({
+		size: {
+			x: 14,
+			y: 14
+		},
+		offset: {
+			x: -1,
+			y: 3
+		},
+		maxVel: {
+			x: 72,
+			y: 900
+		},
+		vel: {
+			x: 64,
+			y: 0
+		},
+		accel: {
+			x: 32,
+			y: 0
+		},
+		bounceSpeed: 150,
+		jumpTime: 1,
+		standTime: 1,
+		dustEmitTime: 0,
+		breakTime: 1,
+		collisionCounter: 0,
+		airCollisionCounter: 0,
+		attacked: false,
+		jumped: false,
+		velGround: 32,
+		velAir: 32,
+		deathTimer: false,
+		type: ig.Entity.TYPE.A, // Player friendly group
+		checkAgainst: ig.Entity.TYPE.NONE,
+		collides: ig.Entity.COLLIDES.PASSIVE,
+		animSheet: new ig.AnimationSheet('med/spr/player.png', 16, 18),
+		deathFallSnd: new ig.Sound('med/sfx/death-fall.*'),
+		bumpSnd: [
             new ig.Sound('med/sfx/bump-00.*'),
             new ig.Sound('med/sfx/bump-01.*'),
             new ig.Sound('med/sfx/bump-02.*')
         ],
-        drawSnd: [
-            new ig.Sound('med/sfx/draw-00.*'),
-            new ig.Sound('med/sfx/draw-01.*'),
-            new ig.Sound('med/sfx/draw-02.*')
-        ],
-        landSnd: [
-            new ig.Sound('med/sfx/land-00.*'),
-            new ig.Sound('med/sfx/land-01.*'),
-            new ig.Sound('med/sfx/land-02.*')
-        ],
-        jumpSnd: [
-            new ig.Sound('med/sfx/jump-00.*'),
-            new ig.Sound('med/sfx/jump-01.*'),
-            new ig.Sound('med/sfx/jump-02.*')
-        ],
-        bounceSnd: [
-            new ig.Sound('med/sfx/bounce-00.*'),
-            new ig.Sound('med/sfx/bounce-01.*'),
-            new ig.Sound('med/sfx/bounce-02.*')
-        ],
-        health: 5,
-        init: function (x, y, settings) {
-            this.parent(x, y, settings);
+		health: 5,
+		init: function (x, y, settings) {
+			this.parent(x, y, settings);
 
-            // Add the animations
-            this.addAnim('run', 0.18, [2, 3]);
-            this.addAnim('backtrack', 0.06, [2, 3]);
-            this.addAnim('idle', 0.34, [0, 1]);
-            this.addAnim('poke', 0.28, [3, 4]);
-            this.addAnim('jump', 1, [5], true);
-            this.addAnim('fall', 1, [8], true);
-            this.addAnim('attack', 1, [6], true);
-            this.addAnim('pain', 1, [7], true);
+			// Add the animations
+			this.addAnim('run', 0.18, [2, 3]);
+			this.addAnim('backtrack', 0.06, [2, 3]);
+			this.addAnim('idle', 0.34, [0, 1]);
+			this.addAnim('poke', 0.28, [3, 4]);
+			this.addAnim('jump', 1, [5], true);
+			this.addAnim('fall', 1, [8], true);
+			this.addAnim('attack', 1, [6], true);
+			this.addAnim('pain', 1, [7], true);
 
-            // Set a reference to the player on the game instance
-            ig.game.player = this;
-        },
+			// Set a reference to the player on the game instance
+			ig.game.player = this;
+		},
 
 
-        update: function () {
-            this.handleOffscreen();
-            this.handleMovement();
-            this.handleAnims();
-            if (this.deathTimer) {
-                if (this.deathTimer.delta() > 0) {
-                    this.kill();
-                }
-            }
-            this.parent();
-        },
-        handleOffscreen: function () {
-            if (this.pos.y > 70) {
-                this.deferredKill(2.32);
-            }
-        },
-        deferredKill: function (wait) {
-            if (!this.deathTimer) {
-                ig.music.fadeOut(1.7);
-                this.deathFallSnd.play();
-                this.deathTimer = new ig.Timer(wait);
-            }
-        },
-        kill: function () {
-            ig.scene.set(ig.SceneRun, {
-                level: {
-                    start: ig.LevelStart,
-                    end: ig.LevelEnd,
-                    pieces: [
+		update: function () {
+			this.handleOffscreen();
+			this.handleMovement();
+			this.handleAnims();
+			if (this.deathTimer) {
+				if (this.deathTimer.delta() > 0) {
+					this.kill();
+				}
+			}
+			this.parent();
+		},
+		handleOffscreen: function () {
+			if (this.pos.y > 70) {
+				this.deferredKill(2.32);
+			}
+		},
+		deferredKill: function (wait) {
+			if (!this.deathTimer) {
+				ig.music.fadeOut(1.7);
+				this.deathFallSnd.play();
+				this.deathTimer = new ig.Timer(wait);
+			}
+		},
+		kill: function () {
+			ig.scene.set(ig.SceneRun, {
+				level: {
+					start: ig.LevelStart,
+					end: ig.LevelEnd,
+					pieces: [
                         ig.LevelOther,
                         ig.LevelSegment00,
                         ig.LevelSegment01,
@@ -130,157 +112,170 @@ ig.module(
                         ig.LevelSegment06,
                         ig.LevelSegment07
                     ],
-                    length: 25,
-                    checkX: false,
-                    checkY: false
-                }
-            });
-            this.parent();
-        },
-        handleAnims: function () {
-            if ((this.standing && this.breakTime > 0) || (this.standing && this.vel.x < 0)) {
-                this.currentAnim = this.anims.backtrack;
-            } else if (this.standing && this.vel.x === 0) {
-                this.currentAnim = this.anims.idle;
-            } else if (this.standing) {
-                this.currentAnim = this.anims.run;
-            } else if (this.attacked) {
-                this.currentAnim = this.anims.attack;
-            } else if (!this.standing && this.vel.y < 0) {
-                this.currentAnim = this.anims.jump;
-            } else if (!this.standing && this.currentAnim !== this.anims.fall) {
-                this.currentAnim = this.anims.fall;
-            }
-        },
-        handleMovement: function () {
-            if (!this.standing) {
-                if ((this.vel.x === 0 && this.airCollisionCounter === 0) || (this.vel.y === 0 && this.airCollisionCounter === 0)) {
-                    this.bumpSnd.random().play();
-                    this.airCollisionCounter += 1;
-                }
-            }
+					length: 25,
+					checkX: false,
+					checkY: false
+				}
+			});
+			this.parent();
+		},
+		handleAnims: function () {
+			if ((this.standing && this.breakTime > 0) || (this.standing && this.vel.x < 0)) {
+				this.currentAnim = this.anims.backtrack;
+			} else if (this.standing && this.vel.x === 0) {
+				this.currentAnim = this.anims.idle;
+			} else if (this.standing) {
+				this.currentAnim = this.anims.run;
+			} else if (this.attacked) {
+				this.currentAnim = this.anims.attack;
+			} else if (!this.standing && this.vel.y < 0) {
+				this.currentAnim = this.anims.jump;
+			} else if (!this.standing && this.currentAnim !== this.anims.fall) {
+				this.currentAnim = this.anims.fall;
+			}
+		},
+		handleMovement: function () {
+			if (!this.standing) {
+				if ((this.vel.x === 0 && this.airCollisionCounter === 0) || (this.vel.y === 0 && this.airCollisionCounter === 0)) {
+					this.bumpSnd.random().play();
+					this.airCollisionCounter += 1;
+				}
+			}
 
-            if (this.standing && this.vel.x === 0 && this.collisionCounter === 1) {
-                this.vel.y = -92;
-                this.accel.x = 32;
-                this.jumped = true;
-                this.vel.x = 48;
-                this.collisionCounter = 0;
-                this.jumpSnd.random().play();
-                return;
-            } else if (this.standing && this.vel.x === 0) {
-                this.vel.x = -32;
-                this.collisionCounter += 1;
-                this.bumpSnd.random().play();
-                return;
-            }
+			if (this.standing && this.vel.x === 0 && this.collisionCounter === 1) {
+				this.vel.y = -92;
+				this.accel.x = 32;
+				this.jumped = true;
+				this.vel.x = 48;
+				this.collisionCounter = 0;
+				//this.jumpSnd.random().play();
+				ig.game.spawnEntity(window.RunParticleJumpEmitter, this.pos.x + 7, this.pos.y + 10);
+				return;
+			} else if (this.standing && this.vel.x === 0) {
+				this.vel.x = -32;
+				this.collisionCounter += 1;
+				this.bumpSnd.random().play();
+				return;
+			}
 
-            if (this.standing && !ig.game.collisionMap.getTile(this.pos.x + this.size.x * 0.9, this.pos.y + this.size.y)) {
-                if (this.vel.x > 32 && this.breakTime < 0.25 && !this.attacked) {
-                    if (this.breakTime === 0) {
-                        this.backtrackSnd.random().play();
-                    }
-                    this.friction.x = 160;
-                    this.breakTime += ig.system.tick;
-                    this.accel.x = 0;
-                } else {
-                    this.accel.x = 10;
-                }
-            } else {
-                this.breakTime = 0;
-                this.friction.x = 0;
-                this.accel.x = 64;
-            }
-            if (!this.standing && ig.input.pressed('click') && !this.attacked && this.jumped) {
-                this.bounceSpeed = -ig.system.height + this.last.y;
-                if (this.bounceSpeed < -90) {
-                    this.attacked = true;
-                    this.jumped = false;
-                    this.drawSnd.random().play();
-                }
-            }
-            if (this.jumped && this.standing) {
-                this.vel.x = 48;
-                this.accel.x = 96;
-                this.jumped = false;
-                this.landSnd.random().play();
-            }
-            if (this.standing) {
-                this.jumpTime = 0;
-                this.airCollisionCounter = 0;
-                if (ig.input.pressed('click') && !this.attacked) {
-                    if (this.vel.x > 0) {
-                        this.vel.y = -138;
-                        this.accel.x = 32;
-                        this.jumped = true;
-                        if (this.vel.x < 48) {
-                            this.vel.x = 48;
-                        }
-                    } else {
-                        this.vel.y = -110;
-                        this.vel.x = 48;
-                        this.accel.x = 64;
-                        this.jumped = true;
-                    }
-                    this.jumpSnd.random().play();
-                }
+			if (this.standing && !ig.game.collisionMap.getTile(this.pos.x + this.size.x * 0.9, this.pos.y + this.size.y)) {
+				if (this.vel.x > 32 && this.breakTime < 0.25 && !this.attacked) {
+					if (this.breakTime === 0) {
+						ig.game.spawnEntity(window.RunParticleBacktrackEmitter, this.pos.x + 7, this.pos.y + 10);
+					}
+					this.friction.x = 180;
+					this.breakTime += ig.system.tick;
+					this.accel.x = 0;
+				} else {
+					this.accel.x = 10;
+				}
+			} else {
+				this.breakTime = 0;
+				this.friction.x = 0;
+				this.accel.x = 64;
+			}
+			if (!this.standing && ig.input.pressed('click') && !this.attacked && this.jumped) {
+				this.bounceSpeed = -ig.system.height + this.last.y;
+				if (this.bounceSpeed < -94) {
+					this.attacked = true;
+					this.jumped = false;
+					//this.drawSnd.random().play();
+					ig.game.spawnEntity(window.RunParticleDrawEmitter, this.pos.x + 3, this.pos.y - 4);
+				}
+			}
+			if (this.jumped && this.standing) {
+				this.vel.x = 48;
+				this.accel.x = 96;
+				this.jumped = false;
+				//this.landSnd.random().play();
+				ig.game.spawnEntity(window.RunParticleLandEmitter, this.pos.x + 7, this.pos.y + 10);
+			}
+			if (this.standing) {
+				this.jumpTime = 0;
+				this.airCollisionCounter = 0;
+				//emit
+				this.dustEmitTime += ig.system.tick;
+				if (this.dustEmitTime > 0.36) {
+					this.dustEmitTime = 0;
+					ig.game.spawnEntity(window.RunParticleRunEmitter, this.pos.x + (Math.floor(Math.random() * 3) - 3), this.pos.y + 11);
+				}
+				if (ig.input.pressed('click') && !this.attacked) {
+					if (this.vel.x > 0) {
+						this.vel.y = -138;
+						this.accel.x = 32;
+						this.jumped = true;
+						if (this.vel.x < 48) {
+							this.vel.x = 48;
+						}
+					} else {
+						this.vel.y = -110;
+						this.vel.x = 48;
+						this.accel.x = 64;
+						this.jumped = true;
+					}
+					//this.jumpSnd.random().play();
+					ig.game.spawnEntity(window.RunParticleJumpEmitter, this.pos.x + 7, this.pos.y + 10);
+				}
 
-                if (this.attacked) {
-                    this.attacked = false;
-                    this.jumped = true;
-                    this.accel.x = 32;
-                    this.vel.y = (this.bounceSpeed * 1.2).limit(-145, 0);
-                    if (this.vel.x < 48) {
-                        this.vel.x = 48;
-                    }
-                    this.bounceSnd.random().play();
-                }
+				if (this.attacked) {
+					this.attacked = false;
+					this.jumped = true;
+					this.accel.x = 32;
+					this.vel.y = (this.bounceSpeed * 1.2).limit(-145, 0);
+					if (this.vel.x < 48) {
+						this.vel.x = 48;
+					}
+					
+					//ig.game.spawnEntity(window.RunParticleBounce, this.pos.x + 7, this.pos.y + 10); //0.034
+					ig.game.spawnEntity(window.RunParticleBounceEmitter, this.pos.x + 7, this.pos.y + 10); //0.034
+					//ig.Timer.timeScale = 0.1;
+				}
 
-            }
-            if (this.attacked) {
-                if (this.vel.y < 128) {
-                    this.vel.y = 128;
-                }
-                this.accel.y = 600;
-                return;
-            }
-            if (this.vel.y > 0) {
-                this.jumped = true;
-                this.collisionCounter = 0;
-                this.standTime = 0;
-                if (this.accel.x < 0) {
-                    this.accel.x = 0;
-                }
-            } else {
-                this.standTime += ig.system.tick;
-            }
-            if (this.vel.y < 0 && ig.input.state('click')) {
-                this.jumpTime += ig.system.tick;
-            }
-            if (this.jumpTime > 0.6) {
-                if (this.vel.y < 0) {
-                    this.accel.y = 64;
-                }
-            } else if (!ig.input.state('click')) {
-                this.accel.y = 48;
-            } else if (this.jumpTime > 0 && ig.input.state('click')) {
-                this.accel.y = -32; //The general acceleration of the jump
-            }
-			
+			}
+			if (this.attacked) {
+				if (this.vel.y < 128) {
+					this.vel.y = 128;
+				}
+				this.accel.y = 600;
+				return;
+			}
+			if (this.vel.y > 0) {
+				this.jumped = true;
+				this.collisionCounter = 0;
+				this.standTime = 0;
+				if (this.accel.x < 0) {
+					this.accel.x = 0;
+				}
+			} else {
+				this.standTime += ig.system.tick;
+			}
+			if (this.vel.y < 0 && ig.input.state('click')) {
+				this.jumpTime += ig.system.tick;
+			}
+			if (this.jumpTime > 0.6) {
+				if (this.vel.y < 0) {
+					this.accel.y = 64;
+				}
+			} else if (!ig.input.state('click')) {
+				this.accel.y = 48;
+			} else if (this.jumpTime > 0 && ig.input.state('click')) {
+				this.accel.y = -32; //The general acceleration of the jump
+			}
+
 			if (!ig.input.state('click') && this.jumpTime < 0.25) {
 				if (this.pos.y < 14) {
-                    this.accel.y = 256;
-                }
-            }
-            if (this.pos.y <= -32) {
-                this.accel.y = 72;
-                this.accel.x = 64;
-                this.maxVel.x = 78;
-            } else if (this.pos.y <= -60) {
-                this.accel.y = 84;
-            } else {
-                this.maxVel.x = 72;
-            }
-        }
-    });
+					this.accel.y = 256;
+				}
+			}
+			if (this.pos.y <= -32) {
+				this.accel.y = 72;
+				this.accel.x = 64;
+				this.maxVel.x = 78;
+			} else if (this.pos.y <= -60) {
+				this.accel.y = 84;
+			} else {
+				this.maxVel.x = 72;
+			}
+		}
+	});
 });
