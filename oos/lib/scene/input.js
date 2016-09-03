22a076a6eb9f43f1d0c85c2b90358637263b723a
Felipe Budinich <Felipe Budinich>
2016-09-03 20:33:18 -0300

    input test 2

--- oos/lib/scene/input.js
+++ oos/lib/scene/input.js
@@ -14,13 +14,42 @@ ig.module(
             var context = ig.system.context;
             context.globalCompositeOperation = "source-over";
             context.globalAlpha = 1;
-            //Input test
-            document.addEventListener("swipe", function (ev) {
-                ig.game.inputText = JSON.stringify(ev);
-            });
         },
         update: function () {
             this.parent();
+            // Each time player clicks, record click X and Y coordinates
+            if (ig.input.pressed('click')) {
+                this.gestureStartX = ig.input.mouse.x;
+                this.gestureStartY = ig.input.mouse.y;
+            } else if (ig.input.released('click')) {
+                this.gestureEndX = ig.input.mouse.x;
+                this.gestureEndY = ig.input.mouse.y;
+                this.evaluateGesture();
+            }
+        },
+        evaluateGesture: function () {
+            // Check to make sure swipe was left to right & long enough
+            // By subtracting end/release coordinates from start coordinates
+            // And run any other checks for your action
+            if (this.gestureStartX < this.gestureEndX && this.gestureEndX - this.gestureStartX > ig.system.width / 3) {
+                // Do stuff if all conditions are met
+                this.swipedRight();
+                // Clear the old gesture
+                this.clearGesture();
+            } else {
+                ig.game.inputText = 'not a swipe';
+                this.clearGesture();
+            }
+        },
+        clearGesture: function () {
+            this.gestureEndX = null;
+            this.gestureStartX = null;
+            this.gestureStartY = null;
+            this.gestureEndY = null;
+            this.gesturesReady = false;
+        },
+        swipedRight: function () {
+            ig.game.inputText = 'swiped right';
         },
         draw: function () {
             // Draw all entities and backgroundMaps
