dbf04426860b95b1ea52fa67c93021922bbb273c
Felipe Budinich <Felipe Budinich>
2016-09-03 22:16:56 -0300

    Input test 3

--- oos/lib/scene/input.js
+++ oos/lib/scene/input.js
@@ -9,6 +9,7 @@ ig.module(
     ig.SceneInput = ig.Game.extend({
         font: new ig.Font('med/04b03.font.png'),
         inputText: 'not working',
+        accumulator: 0,
         init: function () {
             //Reset Context
             var context = ig.system.context;
@@ -21,35 +22,72 @@ ig.module(
             if (ig.input.pressed('click')) {
                 this.gestureStartX = ig.input.mouse.x;
                 this.gestureStartY = ig.input.mouse.y;
-            } else if (ig.input.released('click')) {
+            }
+            if (ig.input.released('click')) {
                 this.gestureEndX = ig.input.mouse.x;
                 this.gestureEndY = ig.input.mouse.y;
-                this.evaluateGesture();
             }
+            this.evaluateGesture();
         },
         evaluateGesture: function () {
             // Check to make sure swipe was left to right & long enough
             // By subtracting end/release coordinates from start coordinates
             // And run any other checks for your action
-            if (this.gestureStartX < this.gestureEndX && this.gestureEndX - this.gestureStartX > ig.system.width / 3) {
-                // Do stuff if all conditions are met
-                this.swipedRight();
-                // Clear the old gesture
+            var swipeDistance = 12 * ig.dimensions.ratioConstraint,
+                x1 = this.gestureStartX,
+                x2 = this.gestureEndX,
+                y1 = this.gestureStartY,
+                y2 = this.gestureEndY,
+                angle;
+            if (this.distanceTo(x1, y1, x2, y2) < swipeDistance) {
+                //taping
+                ig.game.inputText = 'tap';
                 this.clearGesture();
-            } else {
-                ig.game.inputText = 'not a swipe';
+            } else if (this.distanceTo(x1, y1, x2, y2) >= swipeDistance) {
+                //swiping
+                angle = this.angleTo(x1, y1, x2, y2);
+
+                ig.game.inputText = angle + ' ';
+
+                if (angle <= -100 && angle >= -170) {
+                    ig.game.inputText += 'izquierda arriba';
+                } else if (angle < -80 && angle > -100) {
+                    ig.game.inputText += 'arriba';
+                } else if (angle >= -80 && angle <= -10) {
+                    ig.game.inputText += 'derecha arriba';
+                } else if (angle > -10 && angle < 10) {
+                    ig.game.inputText += 'derecha';
+                } else if (angle >= 10 && angle <= 80) {
+                    ig.game.inputText += 'derecha abajo';
+                } else if (angle > 80 && angle < 100) {
+                    ig.game.inputText += 'abajo';
+                } else if (angle >= 100 && angle <= 170) {
+                    ig.game.inputText += 'izquierda abajo';
+                } else {
+                    ig.game.inputText += 'izquierda';
+                }
+
                 this.clearGesture();
             }
+
+        },
+        distanceTo: function (x1, y1, x2, y2) {
+            var xd = x1 - x2,
+                yd = y1 - y2;
+            return Math.sqrt(xd * xd + yd * yd);
+        },
+        angleTo: function (x1, y1, x2, y2) {
+            return Math.atan2(
+                y2 - y1,
+                x2 - x1
+            ).toDeg();
         },
         clearGesture: function () {
-            this.gestureEndX = null;
-            this.gestureStartX = null;
-            this.gestureStartY = null;
-            this.gestureEndY = null;
-            this.gesturesReady = false;
-        },
-        swipedRight: function () {
-            ig.game.inputText = 'swiped right';
+            this.gestureEndX = undefined;
+            this.gestureStartX = undefined;
+            this.gestureStartY = undefined;
+            this.gestureEndY = undefined;
+            this.accumulator = 0;
         },
         draw: function () {
             // Draw all entities and backgroundMaps
